c6d797421da340ad6ebe6d7556fe7ebf77ee6c19
Rich Cookson <richard.cookson@brilliantnoise.com>
2017-10-03 16:32:58 +0100

    Set access rules to admin menus for non-admin users

--- functions/admin-menu.php
+++ functions/admin-menu.php
@@ -5,7 +5,18 @@
     }
 
     function isa_remove_menus() {
-      remove_menu_page( 'edit.php' );
+      // Remove for everyone
+      remove_menu_page( 'edit.php' ); // Posts
+      remove_menu_page( 'edit-comments.php' ); // Comments
+
+      // Remove for everyone but Administrators
+      if ( ! current_user_can('manage_options') ) {
+        remove_menu_page( 'themes.php' ); // Appearance
+        remove_menu_page( 'plugins.php' ); // Plugins
+        remove_menu_page( 'users.php' ); // Users
+        remove_menu_page( 'tools.php' ); // Tools
+        remove_menu_page( 'options-general.php' ); // Settings
+      }
     }
   }
 
