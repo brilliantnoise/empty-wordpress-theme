358e75d88f0930ee1046347d580bdd052b0e1586
Rich Cookson <richard.cookson@brilliantnoise.com>
2017-10-05 15:12:15 +0100

    Secured custom REST endpoint for menu so only an authenticated user can view it

--- functions/custom-rest-endpoints.php
+++ functions/custom-rest-endpoints.php
@@ -25,10 +25,15 @@
     return wp_get_nav_menu_items('Side Menu');
   }
 
-  add_action( 'rest_api_init', function () {
-          register_rest_route( 'myroutes', '/menu', array(
-          'methods' => 'GET',
-          'callback' => 'get_menu',
-      ) );
-  } );
+  add_action(
+    'rest_api_init', function () {
+      register_rest_route( 'myroutes', '/menu', array(
+        'methods' => 'GET',
+        'callback' => 'get_menu',
+        'permission_callback' => function () {
+          return current_user_can( 'read' );
+        }
+      )
+    );
+  });
 ?>
