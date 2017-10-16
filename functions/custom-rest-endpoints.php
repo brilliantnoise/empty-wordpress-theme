47d9d71bc34adab3c81c5c5ebeee5edb9374b0e6
Rich Cookson <richard.cookson@brilliantnoise.com>
2017-10-16 12:27:01 +0100

    Amended menu endpoint to allow for tree structure

--- functions/custom-rest-endpoints.php
+++ functions/custom-rest-endpoints.php
@@ -19,20 +19,41 @@
   //
   // $customRestEndpoints = new customRestEndpoints();
 
-  // TODO: Encase in a class and get to work
+  function build_tree( array &$elements, $parentId = 0 ) {
+    $branch = array();
+    foreach ( $elements as &$element )
+    {
+        if ( $element->menu_item_parent == $parentId )
+        {
+            $children = build_tree( $elements, $element->ID );
+            if ( $children )
+                $element->menu_children = $children;
+
+            $branch[$element->ID] = $element;
+            unset( $element );
+        }
+    }
+    return $branch;
+  }
+
+  function tree_nav_menu( $menu_id ) {
+    $items = wp_get_nav_menu_items( $menu_id );
+    return  $items ? build_tree( $items, 0 ) : null;
+  }
+
   function get_menu() {
     # Change 'menu' to your own navigation slug.
-    return wp_get_nav_menu_items('Side Menu');
+    return tree_nav_menu('Side Menu');
   }
 
   add_action(
     'rest_api_init', function () {
-      register_rest_route( 'myroutes', '/menu', array(
+      register_rest_route( 'playbook', '/menu', array(
         'methods' => 'GET',
-        'callback' => 'get_menu',
-        'permission_callback' => function () {
-          return current_user_can( 'read' );
-        }
+        'callback' => 'get_menu'//,
+        // 'permission_callback' => function () {
+        //   return current_user_can( 'read' );
+        // }
       )
     );
   });
