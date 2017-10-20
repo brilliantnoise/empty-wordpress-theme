e92eebbe14302447bb0e6ebda5608290f4cc6698
Rich Cookson <richard.cookson@brilliantnoise.com>
2017-10-20 15:12:54 +0100

    Removed old code

--- functions/custom-rest-endpoints.php
+++ functions/custom-rest-endpoints.php
@@ -26,22 +26,9 @@
     return  $items ? build_tree( $items, 0 ) : null;
   }
 
-  // Provide menu REST API endpoint
   function get_menu() {
     return tree_nav_menu('Side Menu');
   }
-
-  //Customise the page REST API endpoint to add menu ID associated with page
-  // function my_rest_prepare_post( $data, $post, $request ) {
-	//   $_data = $data->data;
-	//   $thumbnail_id = get_post_thumbnail_id( $post->ID );
-	//   $thumbnail = wp_get_attachment_image_src( $thumbnail_id );
-	//   $_data['featured_image_thumbnail_url'] = $thumbnail[0];
-	//   $data->data = $_data;
-	//   return $data;
-  // }
-  //
-  // add_filter( 'rest_prepare_post', 'my_rest_prepare_post', 10, 3 );
 
   add_action(
     'rest_api_init', function () {
